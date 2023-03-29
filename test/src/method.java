728d72d74431fe9f469689d62d00c9e7edbdf283
LGH <1468483560.qq.com>
2023-03-29 22:22:56 +0800

    J2023.3.28,java学习

--- test/src/method.java
+++ test/src/method.java
@@ -15,3 +15,40 @@ public class method {
     }
 }
 // 方法的重载：方法的名称一样、参数列表不一样、返回值不影响；本质就是编译器会通过参数更改名字，也就是签名
+
+//利用函数重载求两个整数和三个整数中的最大值
+class test12{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        System.out.println(max(a, b));
+        System.out.println(max(a, b, c));
+
+    }
+    public static int max(int a, int b){
+        return a > b ? a : b;
+    }
+    public static int max(int a, int b, int c){
+        int max = max(a, b);
+        return max > c ? max : c;
+    }
+}
+//迭代求解斐波那契数
+class test13{
+    public static void main(String[] args) {
+        System.out.println(fib(5));
+    }
+    public static int fib(int n){
+        int f1 = 1;
+        int f2 = 1;
+        int f3 = 1;
+        for (int i = 3; i <= n; i++){
+            f3 = f2 + f1;
+            f1 = f2;
+            f2 = f3;
+        }
+        return f3;
+    }
+
+}
---
+++ test/src/test/java/DiGui.java
@@ -0,0 +1,51 @@
+/**
+ * @Auther King
+ * @Description:
+ */
+
+// 递归思想：自己调用自己，然后有出口(也就是终止条件或者算式的起始条件)
+// 每一次调用都要在栈上开辟函数栈帧
+//    每次递归的时候，这个方法只执行了一部分就去执行另一部分了
+//    归的时候是把当前方法没执行完的部分执行完，归可以return触发，也可以}触发，最先是return触发，也就是函数栈帧的先进后出
+//    递归的次数是一样的
+public class DiGui {
+    public static void main1(int n){
+        if(1 == n) {
+            System.out.println(n);
+            return ;
+        }
+        main1(n-1);
+        System.out.println(n);
+    }
+    public static int function(int n){
+//        设置出口
+        if (1 == n){
+            return 1;
+        }
+        return n*function(n-1);
+    }
+//    打印各位数
+    public static void function2(int n){
+        if (n < 10){
+            System.out.println(n);
+//            这里一定要加return 不然半天找不出问题
+           return ;
+        }
+        function2(n / 10);
+        System.out.println(n % 10);
+    }
+//    各位数的值相加
+    public static int function3(int n){
+        if(n < 10){
+            return n;
+        }
+        return n % 10 + function3(n / 10);
+
+    }
+    public static void main(String[] args) {
+//        main1(10);
+//        System.out.println(function(4));
+//        function2(123);
+        System.out.println(function3(123));
+    }
+}
